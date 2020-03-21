cac82471c716a127222a65f81f1be4d7c4831c42
MerlitaAmelia <merlita.amelia@students.amikom.ac.id>
2020-03-21 13:12:01 +0700

    praktikum3

---
+++ Tugasclassdanobjek/Tugasclassdanobjek/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugasclassdanobjek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "jono";
+            taxi.OnDuty = true;
+            taxi.NumPasangger = 10;
+
+
+            taxi.TaxiInfo();
+            Console.WriteLine();
+            taxi.PickUpPasangger();
+            taxi.DropOffPasangger();
+
+            Console.ReadKey();
+        }
+    }
+}
